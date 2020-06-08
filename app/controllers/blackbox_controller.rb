08a633bd94c080876edfd0c102fd61eb6141a0b3
lotric1 <rgtricardo1@gmail.com>
2020-06-08 13:02:45 -0300

    tentei começar a mostrar as caixas (alterei a view e o controller), mas antes preciso implementar a autenticação corretamente

--- app/controllers/blackbox_controller.rb
+++ app/controllers/blackbox_controller.rb
@@ -1,2 +1,10 @@
 class BlackboxController < ApplicationController
+    def index
+        #@bboxes = Bbox.where(user_id: current_user.id)
+        @bboxes = Bbox.all
+    end
+    def contents (bbox)
+        #@bbox_contents = Bbox_contents.where(user_id: current_user.id, bbox_id: bbox.id)
+        @bbox_contents = BboxContent.all
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bbox1 = Bbox.create(title: "College", position: 1, items_per_day: 3, starred: true, user_id: 1)
+bbox2 = Bbox.create(title: "Personal", position: 2, items_per_day: 3, starred: false, user_id: 1)
